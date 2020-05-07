8ce3b0c179bc6f23003c3569212dd0972639971d
Colin <rodwell.colin@gmail.com>
2020-05-07 06:46:39 -0700

    make another set of item lambdas getting userId from token instead of argument, so user can only see their own items

---
+++ prac2/myitem_lambdas.py
@@ -0,0 +1,136 @@
+import json
+import boto3
+import os
+import json_func
+import user_lambdas
+from boto3.dynamodb.conditions import Key
+
+#myitems - do all the item lambdas but get user id from auth instead of a prarameter
+
+#_____LAMBDAS_____
+
+#add item to a user
+#if user doesn't exist, nextItemNum will error - check that more explicitly in here?
+def add_item(event, context):
+	try:
+		userId = json_func.get_username(event)
+		value = json_func.get_body_args(event, {'value':str})
+		itemNum = user_lambdas.nextItemNum(userId)
+		table = itemTableResource()
+		resp = table.put_item(Item={"userId": userId, "itemId": itemNum, "value": value})
+		return {
+			"statusCode": 201,
+			"body": "added item"
+		}
+	except Exception as err:
+		return json_func.errorMessage(err)
+
+def get_item(event, context):
+	try:
+		userId = json_func.get_username(event)
+		itemId = json_func.get_querystring_args(event, {'itemId':int})
+		table = itemTableResource()
+		resp = table.get_item(Key={"userId": userId, "itemId": itemId})
+		if 'Item' not in resp:
+			raise FileNotFoundError("item with userId = "+userId+", itemId = "+str(itemId)+" does not exist")
+		return{
+			"statusCode": 200,
+			"body": json.dumps(resp['Item'], cls=json_func.DecimalEncoder)
+		}
+	except Exception as err:
+		return json_func.errorMessage(err)
+
+#get all items for the user
+#TODO - allow filters
+#return count and metadata, or just the items list?
+def get_user_items(event, context):
+	try:
+		userId = json_func.get_username(event)
+		table = itemTableResource()
+		resp = table.query(KeyConditionExpression=Key('userId').eq(userId))
+		return{
+			"statusCode": 200,
+			"body": json.dumps(resp, cls=json_func.DecimalEncoder)
+		}
+	except Exception as err:
+		return json_func.errorMessage(err)
+
+#change one attribute of the item, including custom attributes. currently value has to be string.
+#do we want to allow something this general?
+def edit_item_field(event, context):
+	try:
+		userId = json_func.get_username(event)
+		itemId, attrName, attrValue = json_func.get_body_args(event, {'itemId':int, 'attrName':str, 'attrValue':str})
+		if not existingItem(userId, itemId):
+			return{
+				"statusCode": 404,
+				"body": "item with userId = "+userId+", itemId = "+str(itemId)+" does not exist."
+			}
+		editResp = itemTableResource().update_item(
+			Key = {"userId": userId, "itemId": itemId},
+			ExpressionAttributeNames={
+				"#attrName": attrName,
+			},
+			ExpressionAttributeValues={
+				":attrValue": attrValue,
+			},
+			UpdateExpression="SET #attrName = :attrValue",
+		)
+
+		#TODO check the edit response for success, or return the response
+		return{
+			"statusCode": 200,
+			"body": "set attribute '"+attrName+"' to "+repr(attrValue)
+		}
+	except Exception as err:
+		return json_func.errorMessage(err)
+
+#delete one item by userId and itemId
+#version with client.delete_item : client doesn't specify region, deletes from table in the region matching the lambda
+#should this use resource delete_item instead?
+def delete_item(event, context):
+	try:
+		userId = json_func.get_username(event)
+		itemId = json_func.get_body_args(event, {'itemId':int})
+		item_table_name = os.environ['ITEM_TABLE']
+		client = boto3.client('dynamodb')
+		resp = client.delete_item(
+			TableName=item_table_name,
+			Key={
+				'userId':{
+					'S': userId,
+				},
+				'itemId':{
+					'N': str(itemId) 	#should it be entered as string type?
+				}
+
+			},
+			ReturnValues='ALL_OLD')
+		if "Attributes" in resp: #deleted something
+			return{
+				"statusCode": 200,
+				"body": "deleted item: "+json.dumps(resp['Attributes'])
+			}
+		else:
+			return{
+				"statusCode": 404,
+				"body": "item with userId = "+userId+", itemId = "+str(itemId)+" does not exist."
+			}
+	except Exception as err:
+		return json_func.errorMessage(err)
+
+#_____INTERNAL_____
+
+def itemTableResource():
+	item_table_name = os.environ['ITEM_TABLE']
+	region = os.environ['REGION']
+	dynamodb = boto3.resource('dynamodb', region_name=region)
+	table = dynamodb.Table(item_table_name)
+	return table
+
+def existingItem(userId, itemId):
+	table = itemTableResource()
+	resp = table.get_item(Key={"userId": userId, "itemId": itemId})
+	if 'Item' in resp:
+		return resp['Item'] 
+	return None
